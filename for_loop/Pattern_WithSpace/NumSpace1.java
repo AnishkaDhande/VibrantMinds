8df2ca227f510777da33948db061244bf3babdc1
Anishka Dhande <anishkadhande8@gmail.com>
2026-08-23 01:21:31 +0530

    Add files via upload

---
+++ for_loop/Pattern_WithSpace/NumSpace1.java
@@ -0,0 +1,18 @@
+class NumSpace1
+{
+public static void main(String args[])
+  {     
+    for(int i=1;i<=5;i++)
+     {
+        for(int j=i; j<5 ;j++)
+          {
+         System.out.print(" ");
+          }
+        for(int j=i; j>=1 ;j--)//1-1
+          {
+          System.out.print(i); 
+          }
+      System.out.println();
+     }       
+   }
+}
