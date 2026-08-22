e46a95cc2cf35d1cd121b333f1405de3ab23e1d6
Anishka Dhande <anishkadhande8@gmail.com>
2026-08-22 23:32:07 +0530

    Add files via upload

---
+++ for_loop/pattern_WithoutSpace/Star2.java
@@ -0,0 +1,14 @@
+class Star2
+{    
+    public static void main(String args[])
+     {
+       for(int i=5;i>=1;i--)
+         {
+             for(int j=i; j>=1 ;j--)//5-1
+                 {
+             System.out.print("*");
+                 }
+           System.out.println();
+          }
+      }
+}
