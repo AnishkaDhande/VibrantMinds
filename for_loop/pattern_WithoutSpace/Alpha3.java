f73ccda090be6f2829fd08426d6452498fbfcf24
Anishka Dhande <anishkadhande8@gmail.com>
2026-08-22 23:22:39 +0530

    Add files via upload

---
+++ for_loop/pattern_WithoutSpace/Alpha3.java
@@ -0,0 +1,14 @@
+class Alpha3
+{    
+    public static void main(String args[])
+     {    int n=5;
+       for(int i=1;i<=5;i++)
+         {
+             for(int j=1;j<=i;j++)//1-1
+                 {
+             System.out.print((char)(64+j));
+                 }
+           System.out.println();
+          }
+      }
+}
