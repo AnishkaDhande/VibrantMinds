ddfea70439e0aa3c605fd2a9f04a29f06e35158c
Anishka Dhande <anishkadhande8@gmail.com>
2026-09-12 23:33:20 +0530

    Add files via upload

---
+++ for_loop/Counter_Programs/with_Space/CounterSpace21.java
@@ -0,0 +1,24 @@
+class CounterSpace21
+{
+   public static void main()
+    {
+        int n=5;
+        int count=(n*(n+1))/2;
+        for(int i=1;i<=n;i++)
+         {
+           int c=count;
+           for(int j=i;j>1;j--)
+            {
+              System.out.print("\t");
+            }
+           for(int j=n;j>=i;j--)
+            {
+              System.out.print(c);
+              System.out.print("\t");
+               c-=j;
+            }
+            System.out.println();
+            count--;
+         }
+    }
+}
