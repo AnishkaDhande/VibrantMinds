24eac6fbbf48560851902c9347fc3de5991706c7
Anishka Dhande <anishkadhande8@gmail.com>
2026-09-15 18:17:22 +0530

    Add files via upload

---
+++ for_loop/Counter_Programs/Alphabates/CounterSpaceAlpha2.java
@@ -0,0 +1,23 @@
+class CounterSpaceAlpha2
+{
+  public static void main(String args[])
+   {
+     int n=5;
+     int count=1;
+     for(int i=1;i<=n;i++)
+       {
+         int c=count;
+           for(int j=i;j>1;j--)
+            {
+              System.out.print(" ");
+            }
+           for(int j=n;j>=i;j--)
+            {
+             System.out.print((char)(96+c));
+             c+=j;
+            }
+         System.out.println();
+         count++;
+       }
+   }
+}
