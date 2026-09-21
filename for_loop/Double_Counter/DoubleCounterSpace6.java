fc398c08004bc595306c7145e70c94ab92ea66e4
Anishka Dhande <anishkadhande8@gmail.com>
2026-09-21 14:06:09 +0530

    Add files via upload

---
+++ for_loop/Double_Counter/DoubleCounterSpace6.java
@@ -0,0 +1,29 @@
+class DoubleCounterSpace6
+{
+  public static void main(String args[])
+   {
+     int n=5;
+     int count1=n;
+     int count2=(n*(n+1))/2;
+    for(int i=1;i<=n;i++)
+     {
+       int c1=count1;
+       int c2=count2;
+        for(int j=i;j>1;j--)
+          {
+         System.out.print("\t");
+          }
+        for(int j=n,j1=i; j>=i && j1<=n ;j--,j1++)
+          {
+           System.out.print((char)(64+c1));
+           System.out.print(c2);
+           System.out.print("\t");
+            c1+=j-1;
+            c2-=j1+1;
+          }
+         System.out.println();
+         count1--;
+         count2-=i;
+     }
+   }
+}
