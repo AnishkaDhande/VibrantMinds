6c81c1d1d00a8af0e22c955475e842efe5e55ce9
Anishka Dhande <anishkadhande8@gmail.com>
2026-09-22 23:12:23 +0530

    Add files via upload

---
+++ for_loop/Special_Program/Special1.java
@@ -0,0 +1,13 @@
+class Special1
+{
+  public static void main(String args[])
+  {
+    for(int i=1;i<=10;i++)
+     {
+       if(i%2!=0)
+         System.out.println(1);
+       else
+         System.out.println(0);
+     }
+  }
+}
