a59bb5107bd0ab0d775fcc8d7f27051226d4fc80
Anishka Dhande <anishkadhande8@gmail.com>
2026-09-24 14:15:56 +0530

    Add files via upload

---
+++ for_loop/Rectangle_pattern/Rect_5.java
@@ -0,0 +1,16 @@
+class Rect_5
+{
+  public static void main(String args[])
+   {
+     int n=5;
+     for(int i=1;i<=n;i++)
+       {
+           for(int j=n;j>=i;j--)
+              System.out.print(j);
+           for(int j=i;j>1;j--)
+              System.out.print(i);
+
+          System.out.println();
+       }
+   }
+}
